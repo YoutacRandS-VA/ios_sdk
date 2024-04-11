3df09ef585272a45a66a1bebca3fea036508a2b3
Aditi3 <Aditi3@users.noreply.github.com>
2024-04-11 12:43:22 +0530

    refac: rename linkMeEnabled boolean

--- Adjust/ADJActivityHandler.m
+++ Adjust/ADJActivityHandler.m
@@ -2025,7 +2025,7 @@ remainsPausedMessage:(NSString *)remainsPausedMessage
 - (void)checkLinkMeI:(ADJActivityHandler *)selfI {
 #if TARGET_OS_IOS
     if (@available(iOS 15.0, *)) {
-        if (selfI.adjustConfig.linkMeEnabled == NO) {
+        if (selfI.adjustConfig.isLinkMeEnabled == NO) {
             [self.logger debug:@"LinkMe not allowed by client"];
             return;
         }
