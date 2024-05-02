008e2d9ea4855e274abc93a457a033dc7110cf47
nonelse <pedro@adjust.com>
2024-05-02 10:30:16 +0200

    Feat: Move coppa compliance from config to api

--- Adjust/ADJPackageBuilder.h
+++ Adjust/ADJPackageBuilder.h
@@ -93,7 +93,8 @@
                    forActivityKind:(ADJActivityKind)activityKind
                      withAttStatus:(NSString * _Nullable)attStatusString
                      configuration:(ADJConfig * _Nullable)adjConfig
-                     packageParams:(ADJPackageParams * _Nullable)packageParams;
+                     packageParams:(ADJPackageParams * _Nullable)packageParams
+                     activityState:(ADJActivityState *_Nullable)activityState;
 
 + (void)removeConsentDataFromParameters:(nonnull NSMutableDictionary *)parameters;
 
